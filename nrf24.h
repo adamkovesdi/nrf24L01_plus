5204542560491f0317cedc11d5a7caada6d2c6ba
Adam <adamkov@local>
2021-01-07 11:10:07 +0100

    adamkov compatibility changes for TMRh20 lib

--- nrf24.h
+++ nrf24.h
@@ -1,61 +1,65 @@
 /*
-* ----------------------------------------------------------------------------
-* “THE COFFEEWARE LICENSE” (Revision 1):
-* <ihsan@kehribar.me> wrote this file. As long as you retain this notice you
-* can do whatever you want with this stuff. If we meet some day, and you think
-* this stuff is worth it, you can buy me a coffee in return.
-* -----------------------------------------------------------------------------
-* This library is based on this library: 
-*   https://github.com/aaronds/arduino-nrf24l01
-* Which is based on this library: 
-*   http://www.tinkerer.eu/AVRLib/nRF24L01
-* -----------------------------------------------------------------------------
-*/
+ * ----------------------------------------------------------------------------
+ * “THE COFFEEWARE LICENSE” (Revision 1):
+ * <ihsan@kehribar.me> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a coffee in return.
+ * -----------------------------------------------------------------------------
+ * This library is based on this library:
+ *   https://github.com/aaronds/arduino-nrf24l01
+ * Which is based on this library:
+ *   http://www.tinkerer.eu/AVRLib/nRF24L01
+ * -----------------------------------------------------------------------------
+ */
 #ifndef NRF24
 #define NRF24
 
 #include "nRF24L01.h"
 #include <stdint.h>
 
+#ifndef LOW
 #define LOW 0
+#endif
+#ifndef HIGH
 #define HIGH 1
+#endif
 
 #define nrf24_ADDR_LEN 5
-#define nrf24_CONFIG ((1<<EN_CRC)|(0<<CRCO))
+#define nrf24_CONFIG ((1<<EN_CRC)|(1<<CRCO))
 
 #define NRF24_TRANSMISSON_OK 0
 #define NRF24_MESSAGE_LOST   1
 
 /* adjustment functions */
-void    nrf24_init();
+void    nrf24_init(void);
 void    nrf24_rx_address(uint8_t* adr);
 void    nrf24_tx_address(uint8_t* adr);
 void    nrf24_config(uint8_t channel, uint8_t pay_length);
 
 /* state check functions */
-uint8_t nrf24_dataReady();
-uint8_t nrf24_isSending();
-uint8_t nrf24_getStatus();
-uint8_t nrf24_rxFifoEmpty();
+uint8_t nrf24_dataReady(void);
+uint8_t nrf24_isSending(void);
+uint8_t nrf24_getStatus(void);
+uint8_t nrf24_rxFifoEmpty(void);
 
 /* core TX / RX functions */
 void    nrf24_send(uint8_t* value);
 void    nrf24_getData(uint8_t* data);
 
 /* use in dynamic length mode */
-uint8_t nrf24_payloadLength();
+uint8_t nrf24_payloadLength(void);
 
 /* post transmission analysis */
-uint8_t nrf24_lastMessageStatus();
-uint8_t nrf24_retransmissionCount();
+uint8_t nrf24_lastMessageStatus(void);
+uint8_t nrf24_retransmissionCount(void);
 
 /* Returns the payload length */
-uint8_t nrf24_payload_length();
+uint8_t nrf24_payload_length(void);
 
 /* power management */
-void    nrf24_powerUpRx();
-void    nrf24_powerUpTx();
-void    nrf24_powerDown();
+void    nrf24_powerUpRx(void);
+void    nrf24_powerUpTx(void);
+void    nrf24_powerDown(void);
 
 /* low level interface ... */
 uint8_t spi_transfer(uint8_t tx);
@@ -77,7 +81,7 @@ void    nrf24_writeRegister(uint8_t reg, uint8_t* value, uint8_t len);
  *    - Set CSN pin output
  *    - Set CE pin output     */
 /* -------------------------------------------------------------------------- */
-extern void nrf24_setupPins();
+extern void nrf24_setupPins(void);
 
 /* -------------------------------------------------------------------------- */
 /* nrf24 CE pin control function
@@ -109,8 +113,8 @@ extern void nrf24_mosi_digitalWrite(uint8_t state);
 
 /* -------------------------------------------------------------------------- */
 /* nrf24 MISO pin read function
-/* - returns: Non-zero if the pin is high */
+ * - returns: Non-zero if the pin is high */
 /* -------------------------------------------------------------------------- */
-extern uint8_t nrf24_miso_digitalRead();
+extern uint8_t nrf24_miso_digitalRead(void);
 
 #endif
